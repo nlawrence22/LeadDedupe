e8a5952e9046902a20b72305ab8254ca53ae739f
Nicholas Lawrence <nlawrence@wsu.edu>
2017-06-07 13:19:34 -0700

    Define JSON helper class
    We should keep all of our JSON logic separte from the main logic
    of the deduplication process.

---
+++ src/com/nlaw/leadDedupe/JsonFileUtils.java
@@ -0,0 +1,25 @@
+package com.nlaw.leadDedupe;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ *  JsonFileUtils
+ *
+ *  This is a helper class for JSON and File related
+ *  operations.  Since we're typically either reading
+ *  JSON from a file or writing JSON to a file, it
+ *  doesn't make sense to separate our JSON and File
+ *  methods into separate classes at this time.
+ *
+ */
+public interface JsonFileUtils {
+
+    File getInputFile(String inputFilePath);
+
+    File createOutputFile(String outputFilePath);
+
+    List<Lead> parseJson(File inputFile);
+
+    void writeOutputFile();
+}
