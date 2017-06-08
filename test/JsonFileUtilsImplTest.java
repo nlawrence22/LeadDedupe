4f73dccb621762f9268b2d8c7e2d09d54c2f7702
Nicholas Lawrence <nlawrence@wsu.edu>
2017-06-08 11:45:31 -0700

    Test Json Parsing
    Using the example data we were provided, we want to make sure that
    we have the correct number of items and that the items contain
    good data.  We assume that the getters of the Lead object work
    as expected.

--- test/JsonFileUtilsImplTest.java
+++ test/JsonFileUtilsImplTest.java
@@ -1,4 +1,5 @@
 import com.nlaw.leadDedupe.JsonFileUtilsImpl;
+import com.nlaw.leadDedupe.Lead;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -7,6 +8,7 @@ import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -17,6 +19,8 @@ import static org.junit.Assert.assertEquals;
  *  exit abnormally (and likely throw an exception) otherwise.
  */
 public class JsonFileUtilsImplTest {
+    private String workingdir = System.getProperty("user.dir");
+    private String fileSep = System.getProperty("file.separator");
     private JsonFileUtilsImpl fileUtils;
 
     @Rule
@@ -44,5 +48,38 @@ public class JsonFileUtilsImplTest {
         fileUtils.getInputFile(null);
     }
 
+    @Test
+    public void parseJsonReturnsCorrectNumOfItems() throws IOException {
+        String testJsonPath = workingdir + fileSep + "testResources" +
+                fileSep + "leads.json";
+        File testJsonFile = new File(testJsonPath);
 
+        List<Lead> parseOutput = fileUtils.parseJson(testJsonFile);
+
+        assertEquals(10, parseOutput.size());
+    }
+
+    @Test
+    public void parseJsonReturnsCorrectData() throws IOException{
+        String testJsonPath = workingdir + fileSep + "testResources" +
+                fileSep + "leads.json";
+        File testJsonFile = new File(testJsonPath);
+
+        List<Lead> parseOutput = fileUtils.parseJson(testJsonFile);
+        Lead firstRecord = parseOutput.get(0);
+
+        String email = firstRecord.getEmail();
+        String id = firstRecord.get_id();
+        String firstName = firstRecord.getFirstName();
+        String lastName = firstRecord.getLastName();
+        String address = firstRecord.getAddress();
+        String entryDate = firstRecord.getEntryDate();
+
+        assertEquals("jkj238238jdsnfsj23", id);
+        assertEquals("foo@bar.com", email);
+        assertEquals("John", firstName);
+        assertEquals("Smith", lastName);
+        assertEquals("123 Street St", address);
+        assertEquals("2014-05-07T17:30:20+00:00", entryDate);
+    }
 }
